8e24582b900f0522adcce92cf1dd53b3e70d3f6f
Abdulfatai Buari <abuari@sidmach.com>
2023-06-29 16:30:57 +0100

    Fixed typing

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 // Typing Script JS
 var typed = new Typed(".typing", {
-  strings: ["Web Designer", "Web Developer", "Frontend Developer"],
+  strings: [
+    "Web Designer",
+    "Web Developer",
+    "Frontend Developer",
+    "Software Engineer",
+  ],
   typeSpeed: 80,
   backSpeed: 80,
 });
 
 var typed = new Typed(".typing-2", {
-  strings: ["Web Developer", "Web Designer"],
+  strings: [
+    "Web Designer",
+    "Web Developer",
+    "Frontend Developer",
+    "Software Engineer",
+  ],
   typeSpeed: 100,
   backSpeed: 60,
 });
